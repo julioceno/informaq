502754b9b5fe1be09555963bba3ef71455ac7d11
Julio <julio.nepomuceno@eurekalabs.com.br>
2024-01-10 22:20:22 -0300

    [feature]: fitting correctly

--- src/components/PresentationService/index.tsx
+++ src/components/PresentationService/index.tsx
@@ -25,11 +25,11 @@ export function PresentationService({
     : "md:rounded-tr-6xl md:rounded-bl-6xl";
 
   return (
-    <div className="grid grid-cols-3 gap-3 md:gap-10">
+    <div className="grid grid-cols-3 gap-3 md:gap-10 ">
       <img
         src={photo}
         alt="programming"
-        className={`w-full h-36 md:h-96 xl:h-[30rem] 2xl:h-[37rem] object-cover rounded-md ${imageRounded} col-span-3 md:col-span-1 md:${imageOrder}`}
+        className={`w-full h-36 md:h-96 xl:h-[30rem] 2xl:h-[37rem] object-cover rounded-md ${imageRounded} col-span-3 md:col-span-1 md:${imageOrder} 2xl:row-span-2`}
       />
       <div
         className={`flex flex-col gap-1 col-span-3 md:col-span-2 text-center md:text-start mt-3 md:mt-0 md:${contentOrder} md:place-self-start`}
@@ -45,7 +45,7 @@ export function PresentationService({
           ))}
         </div>
       </div>
-      <div className="col-span-3 grid md:grid-cols-2 gap-4 order-last">
+      <div className="col-span-3 2xl:col-span-2 grid md:grid-cols-2 gap-4 order-3 mt-5 md:mt-0">
         {features.map((item, index) => (
           <ServiceFeature
             title={item.title}
