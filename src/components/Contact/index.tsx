79ec0c1a02f8729cdaad538ad2a2012a7dd84577
Julio <julio.nepomuceno@eurekalabs.com.br>
2024-01-11 22:37:40 -0300

    [feature]: adjusting layout

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -3,11 +3,11 @@ import { contacts } from "../../utils";
 
 export function Contact() {
   return (
-    <div className="flex flex-col gap-5 font-inter bg-neutral-700 w-10/12 mx-auto rounded-tr-6xl rounded-bl-6xl p-2 pt-5 pb-10 md:py-10 md:px-16 ">
+    <div className="flex flex-col gap-5 font-inter bg-neutral-700 w-10/12 mx-auto rounded-tr-6xl rounded-bl-6xl p-2 pt-5 pb-10 lg:pb-20 md:py-10 md:px-16 ">
       <h2 className=" text-2xl sm:text-3xl md:text-4xl 2xl:text-5xl font-extrabold text-center text-white">
         Entre em contato conosco através de alguma das formas disponiveis
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2">
+      <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-4">
         <p className="text-white text-center md:text-start">
           Não deixe seus problemas tecnológicos se acumularem, estamos aqui para
           simplificar sua vida digital. Experimente a excelência em serviços de
